034fbe642f86025ad64d683978cf5b79ead321b6
maryartkey <maryartkey@gmail.com>
2024-05-13 12:08:50 +0700

    Updated requirements.txt

--- website/views.py
+++ website/views.py
@@ -12,353 +12,183 @@ from collections import Counter
 import pickle
 import joblib
 
-with open('model.pkl', 'rb') as f:
-    clf2 = joblib.load(f)
+with open('model_nohomo.pkl', 'rb') as f:
+    regr = pickle.load(f)
 
 class TextGroup:
     def __init__(self, texts):
         self._texts = texts  
+        self.nlp = spacy.load("en_core_web_sm")
 
     def get_mean_word_length(self):
         texts = self._texts
-        mean_word_lengths = []
-
-        for text in texts['text']:
-            words_length = 0
-            number_of_words = 0
-            for word in text.split():
-                words_length += len(word)
-                number_of_words += 1
-            if number_of_words != 0:
-                mean_word_lengths.append(words_length/number_of_words)
-            else:
-                mean_word_lengths.append(1)
-        return mean_word_lengths
+        mean_word_length = 0
+        number_of_words = 0
+        words_length = 0
+        for word in texts.split():
+            words_length += len(word)
+            number_of_words += 1
+        if number_of_words != 0:
+            mean_word_length = (words_length/number_of_words)
+        else:
+            mean_word_length = 1
+        return mean_word_length
     
     def get_stdev_word_lengths(self):
         texts = self._texts
-        stdevs = []
-
-        def stdev_text(text):
-            lengths = list(map(len, text.split()))
-            values = 0
-            if lengths != []:
-                values = st.stdev(lengths)            
-            return values
-        for text in texts['text']:
-            if text != []:
-                stdevs.append(stdev_text(text))    
-            else:
-                stdevs.append(0)             
-        result_values = []
-        for value in stdevs:
-            if value == 0:
-                value = min(stdevs)   
-            result_values.append(value)
-        return result_values
+        mean_word_length = 0
+        number_of_words = 0
+        words_length = 0
+        for word in texts.split():
+            words_length += len(word)
+            number_of_words += 1
+        if number_of_words != 0:
+            mean_word_length = (words_length/number_of_words)
+        else:
+            mean_word_length = 1
+        return mean_word_length
+        
 
     def get_mean_sentence_length(self):      
+        sentences_length = 0
+        number_of_sentences = 0
         texts = self._texts
-        mean_sentence_lengths = []
-
-        for text in texts['text']:
-            sentences_length = 0
-            number_of_sentences = 0
-            for sentence in sent_tokenize(text):                  
-                sentences_length += len(sentence.split())
-                number_of_sentences += 1
-            if number_of_sentences == 0:
-                number_of_sentences = 1
-            if sentences_length == 0:
-                sentences_length = 1
-            mean_sentence_lengths.append(sentences_length/number_of_sentences)
+        for sentence in sent_tokenize(texts):                  
+            sentences_length += len(sentence.split())
+            number_of_sentences += 1
+        if number_of_sentences == 0:
+            number_of_sentences = 1
+        if sentences_length == 0:
+            sentences_length = 1
+        mean_sentence_lengths = (sentences_length/number_of_sentences)
         return mean_sentence_lengths
     
     def get_stdev_sentence_lengths(self):
         texts = self._texts
-        stdevs = []
-
-        def stdev_text(text):
-            lengths = list(map(len, sent_tokenize(text)))
-            values = 0
-            if len(lengths) > 1:
-                values = st.stdev(lengths)
-            return values
-        for text in texts['text']:
-            if text != []:
-                stdevs.append(stdev_text(text))
-            else:
-                stdevs.append(0)
-        result_values = []
-        for value in stdevs:
-            if value == 0:
-                value = min(stdevs)   
-            result_values.append(value)
-        return result_values
+        lengths = list(map(len, sent_tokenize(texts)))
+        values = 0
+        if len(lengths) > 1:
+            values = st.stdev(lengths)
+        return values
     
     def get_dots_count(self):
-        list_values = []
         texts = self._texts
-        
-        for text in texts['text']:
-            marks_in_text = 0
-            other_marks_in_text = 0
-            for word in text.split():
-                if word == '.':
-                    marks_in_text += 1
-                elif word == ',' or word == '!' or word == '?' or word == ':' or word == ';':
-                    other_marks_in_text += 1
-            total_marks = marks_in_text + other_marks_in_text
-            if total_marks == 0:
-                total_marks = 1
-            list_values.append(marks_in_text/total_marks)
+        marks_in_text = 0
+        other_marks_in_text = 0
+        for word in texts.split():
+            if word == '.':
+                marks_in_text += 1
+            elif word == ',' or word == '!' or word == '?' or word == ':' or word == ';':
+                other_marks_in_text += 1
+        total_marks = marks_in_text + other_marks_in_text
+        if total_marks == 0:
+            total_marks = 1
+        list_values = (marks_in_text/total_marks)
         return list_values
 
     def get_commas_count(self):
-        list_values = []
         texts = self._texts
-        
-        for text in texts['text']:
-            marks_in_text = 0
-            other_marks_in_text = 0
-            for word in text.split():
-                if word == ',':
-                    marks_in_text += 1
-                elif word == '.' or word == '!' or word == '?' or word == ':' or word == ';':
-                    other_marks_in_text += 1
-            total_marks = marks_in_text + other_marks_in_text
-            if total_marks == 0:
-                total_marks = 1
-            list_values.append(marks_in_text/total_marks)
+        marks_in_text = 0
+        other_marks_in_text = 0
+        for word in texts.split():
+            if word == ',':
+                marks_in_text += 1
+            elif word == '.' or word == '!' or word == '?' or word == ':' or word == ';':
+                other_marks_in_text += 1
+        total_marks = marks_in_text + other_marks_in_text
+        if total_marks == 0:
+            total_marks = 1
+        list_values = (marks_in_text/total_marks)
         return list_values
     
     def get_excpoints_count(self):
-        list_values = []
         texts = self._texts
-        
-        for text in texts['text']:
-            marks_in_text = 0
-            other_marks_in_text = 0
-            for word in text.split():
-                if word == '!':
-                    marks_in_text += 1
-                elif word == '.' or word == ',' or word == '?' or word == ':' or word == ';':
-                    other_marks_in_text += 1
-            
-            total_marks = marks_in_text + other_marks_in_text
-            if total_marks == 0:
-                total_marks = 1
-            list_values.append(marks_in_text/total_marks)
+        marks_in_text = 0
+        other_marks_in_text = 0
+        for word in texts.split():
+            if word == '!':
+                marks_in_text += 1
+            elif word == ',' or word == '.' or word == '?' or word == ':' or word == ';':
+                other_marks_in_text += 1
+        total_marks = marks_in_text + other_marks_in_text
+        if total_marks == 0:
+            total_marks = 1
+        list_values = (marks_in_text/total_marks)
         return list_values
     
     def get_questmarks_count(self):
-        list_values = []
         texts = self._texts
-        
-        for text in texts['text']:
-            marks_in_text = 0
-            other_marks_in_text = 0
-            for word in text.split():
-                if word == '?':
-                    marks_in_text += 1
-                elif word == '.' or word == ',' or word == '!' or word == ':' or word == ';':
-                    other_marks_in_text += 1
-            total_marks = marks_in_text + other_marks_in_text
-            if total_marks == 0:
-                total_marks = 1
-            list_values.append(marks_in_text/total_marks)
+        marks_in_text = 0
+        other_marks_in_text = 0
+        for word in texts.split():
+            if word == '?':
+                marks_in_text += 1
+            elif word == ',' or word == '.' or word == '!' or word == ':' or word == ';':
+                other_marks_in_text += 1
+        total_marks = marks_in_text + other_marks_in_text
+        if total_marks == 0:
+            total_marks = 1
+        list_values = (marks_in_text/total_marks)
         return list_values
     
     def get_colons_count(self):
-        list_values = []
         texts = self._texts
-        
-        for text in texts['text']:
-            marks_in_text = 0
-            other_marks_in_text = 0
-            for word in text.split():
-                if word == ':':
-                    marks_in_text += 1
-                elif word == '.' or word == ',' or word == '!' or word == '?' or word == ';':
-                    other_marks_in_text += 1
-            total_marks = marks_in_text + other_marks_in_text
-            if total_marks == 0:
-                total_marks = 1
-            list_values.append(marks_in_text/total_marks)
+        marks_in_text = 0
+        other_marks_in_text = 0
+        for word in texts.split():
+            if word == ':':
+                marks_in_text += 1
+            elif word == ',' or word == '.' or word == '!' or word == '?' or word == ';':
+                other_marks_in_text += 1
+        total_marks = marks_in_text + other_marks_in_text
+        if total_marks == 0:
+            total_marks = 1
+        list_values = (marks_in_text/total_marks)
         return list_values
 
     def get_semicolons_count(self):
-        list_values = []
         texts = self._texts
-        
-        for text in texts['text']:
-            marks_in_text = 0
-            other_marks_in_text = 0
-            for word in text.split():
-                if word == ';':
-                    marks_in_text += 1
-                elif word == '.' or word == ',' or word == '!' or word == '?' or word == ':':
-                    other_marks_in_text += 1
-            total_marks = marks_in_text + other_marks_in_text
-            if total_marks == 0:
-                total_marks = 1
-            list_values.append(marks_in_text/total_marks)
-        return list_values
-    
-    def get_homogeneity(self):
-        list_values = []        
-        texts = self._texts
-        
-        # Процесс уникальных слов слева направо
-        def forward(text):
-            text_forward = text[:]
-            unic_words = []
-            unic_words_count = [1]
-            for index in range (1, len(text_forward)):
-                word = text_forward[index]
-                if word not in unic_words:
-                    unic_words_count.append(unic_words_count[index - 1] + 1)
-                    unic_words.append(text_forward[index])
-                else:
-                    unic_words_count.append(unic_words_count[index - 1])
-                    
-            return unic_words_count[:]
-        
-        # Процесс уникальных слов справа налево
-        def reverse(text):
-            text_reversed = text[::-1]
-            unic_words = []
-            unic_words_count = [1]
-            for index in range (1, len(text_reversed)):
-                word = text_reversed[index]
-                if word not in unic_words:
-                    unic_words_count.append(unic_words_count[index - 1] + 1)
-                    unic_words.append(text_reversed[index])
-                else:
-                    unic_words_count.append(unic_words_count[index - 1])
-                    
-            return unic_words_count[:]
-        
-        # Подсчёт численных статистик
-        area = 0
-        sup = 0
-        area_abs = 0
-        for text in texts['text']:
-            area = 0
-            text_forward = forward(text)
-            text_reverse = reverse(text)
-            for i in range(len(text_forward)):
-                diff = text_forward[i] - text_reverse[i]
-                area += diff
-                if sup < abs(diff):
-                    sup = abs(diff)
-                area_abs = abs(area)
-            list_values.append(area_abs)
-
-        return list_values
-
-    def get_average_rank(self):
-        texts = self._texts
-        average_ranks = []
-
-        def build_library_for_rank():
-            library = self._texts
-
-            frequency = Counter()
-            for text in library['text']:
-                frequency.update(text.split())
-            
-            rank_library = dict(sorted(frequency.items(), key=lambda elem: elem[1], reverse=True))                     
-
-            return rank_library
-
-        def rank_for_text(library, text):
-            
-            rank_library = library
-            
-            most_frequent_count = rank_library.keys()
-
-            rank_for_lib = {}
-
-            for i, word in enumerate(most_frequent_count, start=1):
-                rank_for_lib[word] = i
-
-            frequency_in_text = {}
-            all_words = 0
-            mapped_words = {}
-            average_r = {}
-            average_rank = 0
-
-            for word in text.split():
-                if not word in stop_words:
-                    count = frequency_in_text.get(word, 0)
-                    frequency_in_text[word] = count + 1
-
-            frequencies_in_text = dict(sorted(frequency_in_text.items(), key=lambda elem: elem[1], reverse=True))                
-
-            all_words = len(text.split())
-
-            for word in frequencies_in_text.keys():                
-                average_rank += int(frequencies_in_text[word] * rank_for_lib[word]) / all_words
-            return average_rank
-
-        library = build_library_for_rank()
-
-        for text in texts['text']:
-            average_ranks.append(rank_for_text(library, text))
-
-        return average_ranks
-
-    def plot_hist(self, statistic, values):
-        dict_for_dataframe = {str(statistic) : values}
-        mean_word_lengths = pd.DataFrame(dict_for_dataframe)
-        # mean_word_lengths.hist()
-        _ = mean_word_lengths.hist(figsize=(14, 9), bins=25) ## или в виде гистограмм
-        plt.tight_layout()
-        plt.show()
+        marks_in_text = 0
+        other_marks_in_text = 0
+        for word in texts.split():
+            if word == ';':
+                marks_in_text += 1
+            elif word == ',' or word == '.' or word == '!' or word == '?' or word == ':':
+                other_marks_in_text += 1
+        total_marks = marks_in_text + other_marks_in_text
+        if total_marks == 0:
+            total_marks = 1
+        list_values = (marks_in_text/total_marks)
+        return list_values   
+   
 
     def get_pos_count(self):
-        print("Enter pos count.")
-        noun_list = []
-        verb_list = []
-        adj_list = []
-        adv_list = []
-
-        texts = self._texts
-        for text in texts['text']:
-            noun_count = 0
-            verb_count = 0
-            adj_count = 0
-            adv_count = 0
-
-            total_count = 0
-            doc = self.nlp(text)
-            for token in doc:
-                if token.pos_ == 'NOUN':
-                    noun_count += 1
-                elif token.pos_ == 'VERB':
-                    verb_count += 1
-                elif token.pos_ == 'ADJ':
-                    adj_count += 1
-                elif token.pos_ == 'ADV':
-                    adv_count += 1
-                total_count += 1
+        text = self._texts
+        nouns = 0
+        verbs = 0
+        adjs = 0
+        advs = 0
+        total_count = 0
+        doc = self.nlp(text)
+        for token in doc:
+            if token.pos_ == 'NOUN':
+                nouns += 1
+            elif token.pos_ == 'VERB':
+                verbs += 1
+            elif token.pos_ == 'ADJ':
+                adjs += 1
+            elif token.pos_ == 'ADV':
+                advs += 1
+            
+            total_count += 1
             if total_count == 0:
                 total_count == 1
-            noun_list.append(noun_count/total_count)
-            verb_list.append(verb_count/total_count)
-            adj_list.append(adj_count/total_count)
-            adv_list.append(adv_count/total_count)
+            nouns = (nouns/total_count)
+            verbs = (verbs/total_count)
+            adjs = (adjs/total_count)
+            advs = (advs/total_count)
+        return nouns, verbs, adjs, advs
 
-        return noun_list, verb_list, adj_list, adv_list
-    
-    
-    def plot_double_hist(self, statistic_1, values_1, statistic_2, values_2):
-        nat = values_1
-        art = values_2
-        sns.histplot(nat, color='blue', legend='natural')
-        sns.histplot(art, color='orange', legend='artificial')
-        plt.show()
 
 views = Blueprint('views', __name__)
 
@@ -367,13 +197,38 @@ def home():
     if request.method == 'POST':
         text_data = request.form.get('textarea')
         print(len(text_data))
-        new_text = Text(data=text_data, user_id=current_user.id)  #providing the schema for the note 
-        db.session.add(new_text) #adding the note to the database 
-        db.session.commit()
-        avg = average_word_len(text_data)
-        tmp = TextGroup(text_data)
+        # new_text = Text(data=text_data, user_id=current_user.id)  #providing the schema for the note 
+        # db.session.add(new_text) #adding the note to the database 
+        # db.session.commit()
+        text = text_data
+        data = TextGroup(text)        
 
-        return render_template("text.html", user=current_user, text_data=text_data, tmp=tmp)
+        values_mean_w_len = data.get_mean_word_length()
+        values_stdev_w_len = data.get_stdev_word_lengths()
+        values_mean_sent_len = data.get_mean_sentence_length()
+        values_stdev_sent_len = data.get_stdev_sentence_lengths()
+        values_dots = data.get_dots_count()
+        values_commas = data.get_commas_count()
+        values_excpts = data.get_excpoints_count()
+        values_qmarks = data.get_questmarks_count()
+        values_semicolons = data.get_semicolons_count()
+        values_colons = data.get_colons_count()
+        values_nouns, values_verbs, values_adj, values_adv = data.get_pos_count()
+
+        values_dict = {'mean_word_len':  values_mean_w_len, 'stdev_word_len' : values_stdev_w_len, 'mean_sent_len': values_mean_sent_len, 'stdev_sent_len' : values_stdev_sent_len,
+       'dot_count': values_dots, 'comma_count' : values_commas, 'excpoint_count':values_excpts, 'qmark_count':values_qmarks,
+       'semicolon_count':values_semicolons, 'colon_count':values_colons, 'noun':values_nouns, 'verb':values_verbs, 'adj':values_adj, 'adv':values_adv}
+        input = pd.DataFrame(values_dict, index=[0])
+        print(values_dict)
+        prediction = regr.predict(input[0:1])
+        generated = 0
+        if prediction == ['human']:
+            output_text = 'Текст, скорее всего, написан человеком.'
+        else:
+            output_text = 'Похоже, что это сгенерированный текст.'
+        print(output_text)
+
+        return render_template("text.html", user=current_user, text_data=text_data, output_text=output_text, generated=generated)
     
     return render_template("home.html", user=current_user)
 
