3315d0fe886c20ed5a8ec943ba75ee07d8fbe613
Maria Kovalevskaya <35896507+maryartkey@users.noreply.github.com>
2025-05-23 04:17:46 +0700

    Delete extra files

--- website/tmp-stoverfw-api.py
+++
@@ -1,8 +0,0 @@
-import json
-import requests
-
-response = requests.get(
-    'https://api.stackexchange.com/2.3/answers?order=desc&sort=activity&site=stackoverflow')
-
-for data in response.json()['items']:
-    print(data['owner']['link'])
